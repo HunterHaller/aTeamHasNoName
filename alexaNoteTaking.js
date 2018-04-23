24f90ef998dbb39e1a9d2d624659d4996b53fed1
Xinyu0423 <38485279+Xinyu0423@users.noreply.github.com>
2018-04-23 08:39:56 -0600

    Add files via upload

--- alexaNoteTaking.js
+++ alexaNoteTaking.js
@@ -4,59 +4,31 @@ var Alexa = require("alexa-sdk");
 
 //first step(wake up Alexa)
 var handlers = {
-  "HelloIntent": function () {
-      this.response.speak("Hello, How can I help you today");
-      this.emit(":responseReady");
-   },
-  
   "LaunchRequest": function () {
-    this.response.speak("Leave a note.").listen("note"); 
-    /// this.response.speak("Alexa take a note").listen("note"); 
+    this.response.speak("OK, Leave a note. Public or private?").listen("Sorry, please try again"); 
     this.emit(":responseReady");
-   }
-      //leave note privately
-   "privateNode": function(){
-        var password=this value, intent, request;
-      if(password=="123"){
-        this.response.speak("Your password is correct");
-        /// this.response.speak("Your password is correct"); 
-      }else{
-        this.response.speak("Your password is wrong, please try again");
-        this.emit(':responseReady');
-    }
-   }
-   //handle the note leaving(last step)
-   "noteLeaving": function(){
-    this.response.speak("Your note have been leaved ")
-    /// this.response.speak("OK saving your note")
-   }
+   },
 
-  'LanguageIntent': function () {
-    var noteLeaving = this.event.request.intent.slots.language.value;
-    this.response.speak("OK, public or private");
-    /// this.response.speak("Public or private");
+  'noteIntent': function () {
+    var noteLeaving = this.event.request.intent.slots.noteType.value;
+    this.response.speak().listen();
     if(noteLeaving=="public"){
-      this.response.speak("OK, leave a note publicly, what is your note?");
-      /// this.response.speak("OK what is your note");
+      this.response.speak("OK, leave a public note, what is your note").listen();
       this.emit(':responseReady');
-      noteLeaving();
+      var note=this.event.request.intent.slots.noteContent.value;
+      //this.emit(':responseReady');
+      if(note=="buy milk"){
+         this.response.speak("OK, note have been saved "+note).listen();
+      }
     }else if(noteLeaving=="private"){
-      /// We need to code here to take in a username before a password 
-      this.response.speak("OK, what is your password");
-      this.emit(':responseReady');
-      privateNode();
+      this.response.speak("OK, leave a private note").listen();
+      ///private note part, haven't done yet.
     }else{
-      this.response.speak("I cannot understand you, please try again");
-      /// this.response.speak("“I am sorry I do not recognize that name. Please try again to say the name more clearly or use a different name in the database. What is your name?”");
-      this.emit(':responseReady');
+      this.response.speak("Sorry, I can't undertsnand you").listen("please try again");
     }
-    //enter this if it is private note
-
+    
     this.emit(':responseReady');
   },
-
-
-}
 };
 
 
